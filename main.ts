1eaf5458af89f7b6a9e5ab235244a50846eb0696
Dom <dominik.stamm@me.com>
2024-01-29 16:56:40 +0100

    initial state

--- main.ts
+++ main.ts
@@ -1,134 +1,528 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+    ItemView, Menu, Notice, Plugin, Pos, setIcon, TAbstractFile, TagCache, TFile, WorkspaceLeaf
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const LOCAL_TAGS_VIEW: string = "local-tags-view";
+const ALL_VIEW: string = "all-view";
 
-interface MyPluginSettings {
-	mySetting: string;
+enum SortMethods {
+	AtoZ,
+	ZtoA,
+	OccurrenceDescending,
+	OccurrenceAscending,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+type TagGroup = {
+	name: string;
+	value: Pos[];
+};
+
+type TagLeaf = {
+	value: TagGroup;
+	childs: TagGroup[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+type TagTree = TagLeaf[];
 
-	async onload() {
-		await this.loadSettings();
+type CallbackFcn = () => void;
+type CallbackEventFcn = (event: MouseEvent) => void;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+export default class LocalTagsPlugin extends Plugin {
+	onload(): void {
+		// console.log('load plugin') // enable plugin
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerView(LOCAL_TAGS_VIEW, (leaf) => new LocalTagsView(leaf));
+		// this.registerView(CANVAS_VIEW, (leaf) => new CanvasView(leaf));
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: ALL_VIEW,
+			name: "Enable plugin",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				this.onloadLocalTagsView();
+				// this.onloadCanvasView();
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	async onloadLocalTagsView(): Promise<void> {
+		if (this.app.workspace.getLeavesOfType(LOCAL_TAGS_VIEW).length == 0) {
+			await this.app.workspace.getRightLeaf(false).setViewState({
+				type: LOCAL_TAGS_VIEW,
+				active: true,
+			}); // view#onOpen()
+		}
+	}
+
+	onunload(): void {
+		// console.log('unload local-tag plugin'); // disable plugin
+	}
+}
+
+class LocalTagsView extends ItemView {
+	m_sortMethod = 2;
+	m_grouped = false;
+	m_collapsed = false;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+
+		this.onSortTagsClicked = this.onSortTagsClicked.bind(this);
+		this.onGroupTagsClicked = this.onGroupTagsClicked.bind(this);
+		this.onCollapseTagsClicked = this.onCollapseTagsClicked.bind(this);
+	}
+
+	getViewType(): string {
+		return LOCAL_TAGS_VIEW;
+	}
+
+	getDisplayText(): string {
+		return "Local Tags";
+	}
+
+	onSortTagsClicked(event: MouseEvent) {
+		console.log("onSortTagsClicked");
+
+		const menu = new Menu();
+
+		menu.addItem((item) =>
+			item
+				.setTitle("Tag (A - Z)")
+				.setIcon("documents")
+				.setChecked(this.m_sortMethod == 0)
+				.onClick(() => {
+					this.m_sortMethod = 0;
+
+					renderView(
+						sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+						this.containerEl,
+						this.m_grouped,
+						this.onSortTagsClicked,
+						this.onGroupTagsClicked,
+						this.onCollapseTagsClicked
+					);
+				})
+		);
+
+		menu.addItem((item) =>
+			item
+				.setTitle("Tag (Z - A)")
+				.setIcon("documents")
+				.setChecked(this.m_sortMethod == 1)
+				.onClick(() => {
+					this.m_sortMethod = 1;
+
+					renderView(
+						sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+						this.containerEl,
+						this.m_grouped,
+						this.onSortTagsClicked,
+						this.onGroupTagsClicked,
+						this.onCollapseTagsClicked
+					);
+				})
+		);
+
+		menu.addSeparator();
+
+		menu.addItem((item) =>
+			item
+				.setTitle("Häufigkeit (hoch - niedrig)")
+				.setIcon("documents")
+				.setChecked(this.m_sortMethod == 2)
+				.onClick(() => {
+					this.m_sortMethod = 2;
+
+					renderView(
+						sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+						this.containerEl,
+						this.m_grouped,
+						this.onSortTagsClicked,
+						this.onGroupTagsClicked,
+						this.onCollapseTagsClicked
+					);
+				})
+		);
+
+		menu.addItem((item) =>
+			item
+				.setTitle("Häufigkeit (niedrig - hoch)")
+				.setIcon("documents")
+				.setChecked(this.m_sortMethod == 3)
+				.onClick(() => {
+					this.m_sortMethod = 3;
+
+					renderView(
+						sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+						this.containerEl,
+						this.m_grouped,
+						this.onSortTagsClicked,
+						this.onGroupTagsClicked,
+						this.onCollapseTagsClicked
+					);
+				})
+		);
+
+		menu.showAtMouseEvent(event);
+	}
+
+	onGroupTagsClicked() {
+		console.log("onGroupTagsClicked");
+
+		this.m_grouped = !this.m_grouped;
+
+		renderView(
+			sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+			this.containerEl,
+			this.m_grouped,
+			this.onSortTagsClicked,
+			this.onGroupTagsClicked,
+			this.onCollapseTagsClicked
+		);
+	}
+
+	onCollapseTagsClicked() {
+		console.log("onCollapseTagsClicked");
+	}
+
+	async onOpen(): Promise<void> {
+		this.icon = "stamp";
+
+		renderView(
+			sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+			this.containerEl,
+			this.m_grouped,
+			this.onSortTagsClicked,
+			this.onGroupTagsClicked,
+			this.onCollapseTagsClicked
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("file-open", () => {
+				renderView(
+					sortLocalTags(this.getLocalTags(), this.m_sortMethod),
+					this.containerEl,
+					this.m_grouped,
+					this.onSortTagsClicked,
+					this.onGroupTagsClicked,
+					this.onCollapseTagsClicked
+				);
+			})
+		);
+	}
+
+	getLocalTags(): TagGroup[] {
+		const activeFile: TFile | null = this.app.workspace.getActiveFile();
+
+		if (activeFile == null) {
+			return [];
+		}
+
+		const fileCache = this.app.metadataCache.getFileCache(activeFile);
+
+		if (fileCache == null) {
+			return [];
+		}
+
+		const tagCache = fileCache.tags;
+
+		if (tagCache == undefined) {
+			return [];
+		}
+
+		type GroupedTags = { [tag: string]: Pos[] };
+		const groupedTags = tagCache.reduce(
+			(groups: GroupedTags, tagElement) => {
+				const { tag, position } = tagElement;
+
+				if (!groups[tag]) {
+					groups[tag] = [];
+				}
+				groups[tag].push(position);
+				return groups;
+			},
+			{}
+		);
+
+		const tagGroups: TagGroup[] = Object.entries(groupedTags).map(
+			([name, value]) => ({
+				name,
+				value,
+			})
+		);
+
+		return tagGroups;
+	}
+
+	async onClose(): Promise<void> {
+		// console.log('close local-tag view');
+	}
+}
+
+function sortLocalTags(tags: TagGroup[], sortMethod: SortMethods): TagGroup[] {
+	switch (sortMethod) {
+		case SortMethods.AtoZ:
+			tags.sort((a, b) => a.name.localeCompare(b.name));
+			break;
+		case SortMethods.ZtoA:
+			tags.sort((a, b) => b.name.localeCompare(a.name));
+			break;
+		case SortMethods.OccurrenceDescending:
+			tags.sort((a, b) => b.value.length - a.value.length);
+			break;
+		case SortMethods.OccurrenceAscending:
+			tags.sort((a, b) => a.value.length - b.value.length);
+			break;
+	}
+
+	return tags;
+}
+
+// function groupLocalTags(tags: TagGroup[]): TagTree {}
+
+function isTagGroup(tagName: string) {
+	return tagName.includes("/");
+}
+
+function createTagList(el: HTMLDivElement, tag: TagGroup) {
+	const contentTreeItem: HTMLDivElement = el.createDiv({
+		cls: "tree-item",
+	});
+
+	contentTreeItem.createDiv(
+		{
+			cls: "tree-item-self is-clickable outgoing-link-item",
+			attr: { draggable: true },
+		},
+		(el) => {
+			el.createDiv({
+				cls: "tree-item-inner",
+				text: tag.name.replace("#", ""),
+			}).addEventListener("click", () => {
+				const query = `tag:${tag.name} file:"${
+					this.app.workspace.getActiveFile().name
+				}"`;
+				this.app.internalPlugins.plugins[
+					"global-search"
+				].instance.openGlobalSearch(query);
+			});
+			el.createDiv({ cls: "tree-item-flair-outer" }, (el) => {
+				el.createSpan({
+					cls: "tag-pane-tag-count tree-item-flair",
+					text: tag.value.length.toString(),
+				});
+			});
+		}
+	);
+}
+
+function createTreeEl(el: HTMLDivElement, tag: string) {
+	const contentTreeItem: HTMLDivElement = el.createDiv({
+		cls: "tree-item",
+	});
+
+	const tagTokens: string[] = tag.split("/");
+
+	for (let i = 0; i < tagTokens.length; ++i) {
+		console.log(tagTokens[i]);
+	}
+}
+
+function createTagTree(el: HTMLDivElement, tag: TagGroup, tagPath: string) {
+	const contentTreeItem: HTMLDivElement = el.createDiv({
+		cls: "tree-item",
+	});
+
+	const tagTokens: string[] = tag.name
+		.replace("#", "")
+		.replace(tagPath, "")
+		.split("/");
+
+	const currentFullTag = `tag:${tagPath}/${tagTokens[0]}`.replace(
+		/tag:\//,
+		""
+	);
+	console.log("currentFullTag");
+	console.log(currentFullTag);
+
+	console.log(tagTokens);
+
+	console.log(tagTokens[0]);
+	contentTreeItem.createDiv(
+		{
+			cls: "tree-item-self is-clickable mod-collapsible",
+			attr: {
+				style: "margin-left: 0px !important; padding-left: 24px !important;",
+			},
+		},
+		(el) => {
+			console.log(`tag:${tagPath}/${tagTokens[0]}`.replace(/^\//, ""));
+			if (tagTokens.length > 1) {
+				el.createDiv(
+					{
+						cls: "tree-item-icon collapse-icon",
+					},
+					(el) => {
+						setIcon(el, "right-triangle");
+					}
+				);
+			}
+			el.createSpan(
+				{
+					cls: "tree-item-inner",
+					text: tagTokens[0],
+				},
+				(el) => {
+					el.createDiv;
+				}
+			).addEventListener("click", () => {
+				const query =
+					`tag:${tagPath}/${tagTokens[0]}`.replace(/tag:\//, "tag:") +
+					" " +
+					`file:"${this.app.workspace.getActiveFile().name}"`;
+				this.app.internalPlugins.plugins[
+					"global-search"
+				].instance.openGlobalSearch(query);
+			});
+			el.createDiv({ cls: "tree-item-flair-outer" }, (el) => {
+				el.createSpan({
+					cls: "tag-pane-tag-count tree-item-flair",
+					text: tag.value.length.toString(),
+				});
+			});
+		}
+	);
+
+	if (tagTokens.length > 0) {
+		el.createDiv(
+			{
+				cls: "tree-item-children",
+			},
+			(child) => {
+				child.createDiv({
+					attr: {
+						style: "width: 1px; height: 0.1px; margin-bottom: 0px;",
+					},
+				});
+
+				const subTag = tagTokens
+					.slice(1)
+					.reduce((acc, current) => {
+						const previous =
+							acc.length > 0 ? acc[acc.length - 1] : "";
+						const combined = previous
+							? `${previous}/${current}`.replace(/^\//, "")
+							: current;
+						return [...acc, combined];
+					}, [])
+					.pop();
+
+				console.log(subTag);
+				if (subTag) {
+					createTagTree(
+						child,
+						{ name: subTag, value: tag.value },
+						`${tagPath}/${tagTokens[0]}`.replace(/^\//, "")
+					);
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function renderView(
+	tags: TagGroup[],
+	container: Element,
+	groupBtnActive: boolean,
+	onSortTagsClicked: CallbackEventFcn,
+	onGroupTagsClicked: CallbackFcn,
+	onCollapseTagsClicked: CallbackFcn
+): void {
+	container.empty();
+
+	const navHeader: HTMLDivElement = container.createDiv({
+		cls: "nav-header",
+	});
+
+	const navHeaderBtns = navHeader.createDiv({
+		cls: "nav-buttons-container",
+	});
+
+	const sortBtn = navHeaderBtns.createDiv({
+		cls: "clickable-icon nav-action-button",
+		attr: {
+			"aira-label": "Sortierreihenfolge ändern",
+		},
+	});
+	sortBtn.addEventListener("click", (event) => {
+		onSortTagsClicked(event);
+	});
+	setIcon(sortBtn, "arrow-up-narrow-wide");
+
+	const groupBtn = navHeaderBtns.createDiv({
+		cls: "clickable-icon nav-action-button",
+		attr: {
+			"aira-label": "Zeige verschachtelte Tags",
+		},
+	});
+	if (groupBtnActive) {
+		groupBtn.addClass("is-active");
 	}
+	groupBtn.addEventListener("click", () => {
+		onGroupTagsClicked();
+	});
+	setIcon(groupBtn, "folder-tree");
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	const collapseBtn = navHeaderBtns.createDiv({
+		cls: "clickable-icon nav-action-button",
+		attr: {
+			"aira-label": "Alles einklappen",
+			"aria-disabled": !groupBtnActive,
+		},
+	});
+	if (groupBtnActive) {
+		collapseBtn.addEventListener("click", () => {
+			onCollapseTagsClicked();
+		});
 	}
+	setIcon(collapseBtn, "chevrons-down-up");
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+	const pane: HTMLDivElement = container.createDiv({
+		cls: "local-tags-pane node-insert-event",
+		attr: { style: "position: relative;" },
+	});
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	const header: HTMLDivElement = pane.createDiv({
+		cls: "tree-item-self is-clickable",
+		attr: {
+			"aria-label": "Click to collapse",
+			"aria-label-position": "right",
+		},
+	});
+	header.createSpan({ cls: "tree-item-icon collapse-icon" });
+	header.createDiv({
+		cls: "tree-item-inner",
+		text: "Local Tags",
+	});
+	header.createDiv({ cls: "tree-item-flair-outer" }, (el) => {
+		el.createSpan({
+			cls: "tree-item-flair",
+		});
+	});
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+	const content: HTMLDivElement = pane.createDiv({
+		cls: "search-result-container",
+	});
+	content.createDiv({
+		attr: {
+			style: "width: 1px; height: 0.1px; margin-bottom: 0px;",
+		},
+	});
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	for (const tag of tags) {
+		if (groupBtnActive) {
+			createTagTree(content, tag, "");
+		} else {
+			createTagList(content, tag);
+		}
 	}
 }
